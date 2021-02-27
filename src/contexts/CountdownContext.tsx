b609d4f2a47deaee6543476572b4aa852297a4c8
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2021-02-27 19:16:25 -0300

    commit

---
+++ src/contexts/CountdownContext.tsx
@@ -0,0 +1,79 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import { ChallengesContext } from './ChallengesContext';
+
+interface CountdownContextData {
+  minutes: number;
+  seconds: number;
+  hasFinished: boolean;
+  isActive: boolean;
+  resetCountdown: () => void;
+  startCountdown: () => void;
+}
+
+interface CountdownProviderProps {
+  children: ReactNode;
+}
+
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
+
+export function CountdownProvider({ children }: CountdownProviderProps) {
+  const { startNewChallenge } = useContext(ChallengesContext);
+
+  const [time, setTime] = useState(getRandomTime() * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  function getRandomTime() {
+    return Math.floor(Math.random() * (3 - 1) + 1);
+  }
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setHasFinished(false);
+    setTime(getRandomTime() * 60);
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setHasFinished(true);
+      setIsActive(false);
+      startNewChallenge();
+    }
+  }, [isActive, time]);
+
+  return (
+    <CountdownContext.Provider
+      value={{
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        resetCountdown,
+        startCountdown,
+      }}
+    >
+      {children}
+    </CountdownContext.Provider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { Countdown } from '../components/Countdown';
+import { Profile } from '../components/Profile';
+
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
+import styles from '../styles/pages/Home.module.css';
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
+  );
+}
+
+// Server Side Function // Call API
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { level, currentExperience, challengesCompleted } = context.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
