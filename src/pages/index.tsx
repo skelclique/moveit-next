e4db44b45d0ebdce8ddc4a63ad4dceeff4094f8e
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2021-02-28 22:33:38 -0300

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,27 +4,25 @@ import styles from '../styles/pages/Login.module.css';
 
 export default function Login() {
   return (
-    // <div className={styles.background}>
-      <div className={styles.loginContainer}>
-        <Head>
-          <title>Inicio | move.it</title>
-        </Head>
+    <div className={styles.loginContainer}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
 
-        <section>
-          <img src="icons/symbol.svg" />
-          <div>
-            <img src="icons/logo.svg" />
-            <strong>Bem-vindo</strong>
+      <section className={styles.gridContainer}>
+        <img src="icons/symbol.svg" />
+        <div>
+          <img src="icons/logo.svg" />
+          <strong>Bem-vindo</strong>
 
-            <div className={styles.githubContainer}>
-              <img src="icons/github.svg" />
-              <p>
-                Faça login com seu Github <br /> para começar
-              </p>
-            </div>
+          <div className={styles.githubContainer}>
+            <img src="icons/github.svg" />
+            <p>
+              Faça login com seu Github <br /> para começar
+            </p>
           </div>
-        </section>
-      </div>
-    // </div>
+        </div>
+      </section>
+    </div>
   );
 }
